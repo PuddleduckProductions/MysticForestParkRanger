c09d7e14b75ddb40c6b74ea85df84f86626b9a33
Tyler K <tyly04@gmail.com>
2024-01-22 11:50:07 -0800

    Finished ink integration

--- Assets/Scripts/Ink/DialogRenderer.cs
+++ Assets/Scripts/Ink/DialogRenderer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class DialogRenderer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    protected RectTransform dialogParent;
+    protected TextMeshProUGUI dialog;
+    protected Camera worldCamera;
+
+    public void Init() {
+        worldCamera = Camera.main;
+        dialogParent = transform.GetChild(0).GetComponent<RectTransform>();
+        dialog = dialogParent.GetComponentInChildren<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    protected bool TryGetCharacter(string name, out GameObject character) {
+        character = GameObject.Find(name);
+        return character != null;
     }
+
+    public void Render(InkManager.DialogLine line) {
+        // Our current "zero" coordinates for setting dialogParent's global coordinates:
+        dialogParent.localScale = Vector3.one;
+        Vector3 position = this.transform.position;
+        if (line.character != "" && TryGetCharacter(line.character, out GameObject character)) {
+            position = worldCamera.WorldToScreenPoint(character.transform.position);
+        }
+        Debug.Log(position);
+        dialog.text = line.dialog;
+        dialogParent.position = position;
+    }
+
+
 }
--- Assets/Scripts/Ink/InkManager.cs
+++ Assets/Scripts/Ink/InkManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
+using System.Text.RegularExpressions;
 
 public class InkManager : MonoBehaviour
 {
@@ -9,26 +10,86 @@ public class InkManager : MonoBehaviour
     protected TextAsset inkJSONAsset;
 
     protected Story story;
+    bool storyActive = false;
 
-    protected Camera worldCamera;
-
+    #region Dialog Parameters
     [SerializeField]
     protected GameObject dialog;
-    // Start is called before the first frame update
-    void Awake()
-    {
+    protected GameObject dialogInstance;
+    protected DialogRenderer dialogRenderer;
+    public struct DialogLine {
+        public string character { get; }
+        public string dialog { get; }
+        public DialogLine(string character, string dialog) {
+            this.character = character;
+            this.dialog = dialog;
+        }
+    }
+
+    static protected Regex lineMatchRegex;
+    static protected DialogLine LineFromString(string text) {
+        const string lineMatch = @"(?:(?<characterName>[\w\W]*):)?(?<dialog>.*)";
+        if (lineMatchRegex == null) {
+            lineMatchRegex = new Regex(lineMatch);
+        }
+
+        Match match = lineMatchRegex.Match(text);
+        return new DialogLine(match.Groups["characterName"].Value, match.Groups["dialog"].Value);
+    }
+    #endregion
+
+
+    void Awake() {
         story = new Story(inkJSONAsset.text);
-        worldCamera = Camera.main;
+        dialogInstance = GameObject.Instantiate(dialog, transform);
+        dialogRenderer = dialogInstance.GetComponent<DialogRenderer>();
+        dialogRenderer.Init();
+        dialogInstance.SetActive(false);
     }
 
-    public void StartDialog(string name)
-    {
-        story.ChoosePathString(name);
-        DrawDialog();
+    #region Flow Control
+    public void StartDialog(string name) {
+        // Not sure if there's a better way to test this.
+        if (!storyActive) {
+            story.ChoosePathString(name);
+            EvaluateStory();
+            storyActive = true;
+        } else {
+            Debug.LogWarning("Dialog already in progress.");
+        }
     }
 
-    protected void DrawDialog()
-    {
-        
+    public void AdvanceStory() {
+        if (story.canContinue) {
+            EvaluateStory();
+        } else {
+            storyActive = false;
+            dialogInstance.SetActive(false);
+        }
     }
+
+    protected void EvaluateStory()
+    {
+        string text = story.Continue();
+        Debug.Log(text);
+        DialogLine line = LineFromString(text);
+        DrawDialog(line);
+    }
+    #endregion
+
+    #region Dialog Rendering
+    protected bool TryFindCharacter(string name, out GameObject character)
+    {
+        character = GameObject.Find(name);
+        if (character == null) {
+            Debug.LogWarning("Could not find Character " + name);
+        }
+        return character == null;
+    }
+
+    protected void DrawDialog(DialogLine line) {
+        dialogRenderer.Render(line);
+        dialogInstance.SetActive(true);
+    }
+    #endregion
 }
