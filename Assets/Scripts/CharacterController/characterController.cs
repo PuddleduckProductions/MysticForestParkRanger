ec59a0354be65478198f06c636169b044e79e8ab
pyromaniac3 <97550706+pyromaniac3@users.noreply.github.com>
2024-01-31 18:46:47 -0800

    first area environment
    The Dugong is trapped from trash in the water.
    
    I have also added a FBX master blend file, so tyler yell at me if the merge is harder cause of that

--- Assets/Scripts/CharacterController/characterController.cs
+++ Assets/Scripts/CharacterController/characterController.cs
@@ -7,22 +7,23 @@ public class characterController : MonoBehaviour
 {
     CharacterController c;
     Vector2 input;
-
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         c = GetComponent<CharacterController>();
-        
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         c.Move(new Vector3(input.x*Time.deltaTime,0,input.y*Time.deltaTime));
-        
+    
     }
     void OnWalking(InputValue value)
     {
         input = value.Get<Vector2>();
+       // animator.SetBool("walking", true);
     }
 }
