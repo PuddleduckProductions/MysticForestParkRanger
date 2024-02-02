018626063fb805f0427fb493c8d7dadcfc45facd
Tyler K <tyly04@gmail.com>
2024-02-02 12:14:01 -0800

    Fixing build scripts

--- Documentation/build.ps1
+++ Documentation/build.ps1
@@ -1,3 +1,6 @@
 docfx metadata $PSScriptRoot/docfx.json
-bash $PSScriptRoot/docfxfix.sh
+$bashroot=$PSScriptRoot -replace '\\', '/'
+# And for handy bash conversion on Windows:
+$bashroot=$bashroot -replace 'C:/', '/mnt/c/'
+bash $bashroot/docfxfix.sh
 docfx build $PSScriptRoot/docfx.json $args[0]
