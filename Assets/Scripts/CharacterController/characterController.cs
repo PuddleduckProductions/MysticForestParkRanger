d86e3c4b07ce383e4fa1d714adc62f0e3639c978
pyromaniac3 <97550706+pyromaniac3@users.noreply.github.com>
2024-02-01 16:28:04 -0800

    Player Animation Implemented, however it doesn't rotate on directional input

--- Assets/Scripts/CharacterController/characterController.cs
+++ Assets/Scripts/CharacterController/characterController.cs
@@ -7,23 +7,28 @@ public class characterController : MonoBehaviour
 {
     CharacterController c;
     Vector2 input;
-    //private Animator animator;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         c = GetComponent<CharacterController>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         c.Move(new Vector3(input.x*Time.deltaTime,0,input.y*Time.deltaTime));
+
+        // Check if the magnitude of input is greater than a threshold (e.g., 0.1)
+        bool isWalking = input.magnitude > 0.01f;
+
+        // Set the "walking" parameter in the animator based on the input magnitude
+        animator.SetBool("walking", isWalking);
     
     }
     void OnWalking(InputValue value)
     {
         input = value.Get<Vector2>();
-       // animator.SetBool("walking", true);
     }
 }
