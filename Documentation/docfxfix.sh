0c561b0e96d829e81f1184531a50776cccc62245
Tyler K <tyly04@gmail.com>
2024-02-02 11:21:19 -0800

    New attempt

--- Documentation/docfxfix.sh
+++ Documentation/docfxfix.sh
@@ -1,3 +1,5 @@
 repl="s/([nN]ame[A-Za-z]*: )''/\1MysticForestParkRanger/g"
-sed -i -E "${repl}" /api/toc.yml
-sed -i -E "${repl}" /api/MysticForestParkRanger.yml
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Works for whatever reason on Github actions:
+sed -i -E "${repl}" $SCRIPT_DIR/../api/toc.yml
+sed -i -E "${repl}" $SCRIPT_DIR/../api/MysticForestParkRanger.yml
