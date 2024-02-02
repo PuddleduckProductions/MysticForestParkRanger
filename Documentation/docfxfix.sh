cee7bbe0b9e08bea69d06935d53973651c79069e
Tyler K <tyly04@gmail.com>
2024-02-02 11:19:09 -0800

    Trying to fix relative directory errors?

--- Documentation/docfxfix.sh
+++ Documentation/docfxfix.sh
@@ -1,4 +1,3 @@
 repl="s/([nN]ame[A-Za-z]*: )''/\1MysticForestParkRanger/g"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-sed -i -E "${repl}" $SCRIPT_DIR/api/toc.yml
-sed -i -E "${repl}" $SCRIPT_DIR/api/MysticForestParkRanger.yml
+sed -i -E "${repl}" /api/toc.yml
+sed -i -E "${repl}" /api/MysticForestParkRanger.yml
