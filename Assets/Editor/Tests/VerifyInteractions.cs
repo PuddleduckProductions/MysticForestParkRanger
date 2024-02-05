fc165644ddcc7db12d4d63d5530172bc8af11ee2
Tyler K <tyly04@gmail.com>
2024-02-04 19:26:55 -0800

    Verify interaction across build scenes

--- Assets/Editor/Tests/VerifyInteractions.cs
+++ Assets/Editor/Tests/VerifyInteractions.cs
@@ -4,28 +4,50 @@ using NUnit.Framework;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using Interactions;
+using UnityEditor;
 
-public class VerifyInteractions
-{
+namespace InteractionTests {
+    [TestFixture]
+    [TestFixtureSource(typeof(ScenesProvider))]
+    public class VerifyInteractions {
+        private string scenePath;
 
-    [SetUp]
-    public void Setup() {
-        EditorSceneManager.OpenScene("Assets/Scenes/Prototype Zero/PrototypeZero.unity");
-    }
+        public VerifyInteractions(string scenePath) {
+            this.scenePath = scenePath;
+        } 
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [Test]
-    public void ValidateInteractions()
-    {
-        var interactions = GameObject.FindObjectsOfType<Interaction>();
-        foreach (var interaction in interactions) {
-            Assert.IsTrue(interaction.HasInteractionBehavior());
+
+        [OneTimeSetUp]
+        public void Setup() {
+            EditorSceneManager.OpenScene(this.scenePath);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [Test]
+        public void ValidateInteractions() {
+            var interactions = GameObject.FindObjectsOfType<Interaction>();
+            foreach (var interaction in interactions) {
+                Assert.IsTrue(interaction.HasInteractionBehavior(),
+                    $"{EditorSceneManager.GetActiveScene().name} invalid interaction {interaction.type} on {interaction.name}");
+            }
+        }
+
+        [OneTimeTearDown]
+        public void Teardown() {
+            EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);
         }
     }
 
-    [TearDown]
-    public void Teardown() {
-        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);
+    public class ScenesProvider : IEnumerable {
+        public IEnumerator GetEnumerator() {
+            foreach (var scene in EditorBuildSettings.scenes) {
+                if (!scene.enabled || scene.path == null) {
+                    continue;
+                }
+
+                yield return scene.path;
+            }
+        }
     }
 }
