0c5d794015ff2d16dd3ddc174a39cd08c6003468
Tyler K <tyly04@gmail.com>
2024-02-12 22:52:25 -0800

    Drafting water sourcing

--- Assets/Scripts/CharacterController/Interactions/PlantInteractions.cs
+++ Assets/Scripts/CharacterController/Interactions/PlantInteractions.cs
@@ -15,6 +15,21 @@ namespace Interactions.Behaviors {
 
         [HideInInspector]
         public LineRenderer renderer;
+
+        public bool isGrown = false;
+
+        protected Vector3[] ends;
+        protected struct Source {
+            GameObject gameObject;
+            int index;
+
+            public Source(GameObject sourceObject, int index) {
+                gameObject = sourceObject;
+                this.index = index;
+            }
+        }
+        protected Source? source = null;
+
         public WaterPipe(Interaction parent) : base(parent) {
             if (!parent.gameObject.TryGetComponent<LineRenderer>(out renderer)) {
                 renderer = parent.gameObject.AddComponent<LineRenderer>();
@@ -23,29 +38,62 @@ namespace Interactions.Behaviors {
         }
 
         protected void DrawLine() {
+            if (!isGrown) {
+                return;
+            }
             renderer.numCapVertices = 1;
-            Vector3[] arr = new Vector3[] { };
+            ends = new Vector3[] { };
             switch (type) {
                 case PipeType.Straight:
-                    arr = new Vector3[] { new Vector3(-1, 1), new Vector3(1, 1) };
+                    ends = new Vector3[] { new Vector3(-1, 1), new Vector3(1, 1) };
                     break;
                 case PipeType.Corner:
-                    arr = new Vector3[] { new Vector3(-1, 1), new Vector3(0, 1), new Vector3(0, 1, 1) };
+                    ends = new Vector3[] { new Vector3(-1, 1), new Vector3(0, 1), new Vector3(0, 1, 1) };
                     break;
                 case PipeType.FourLeaf:
-                    arr = new Vector3[] { new Vector3(-1, 1), new Vector3(1, 1), new Vector3(0, 1, 1), new Vector3(0, 1, -1) };
+                    ends = new Vector3[] { new Vector3(-1, 1), new Vector3(1, 1), new Vector3(0, 1, 1), new Vector3(0, 1, -1) };
                     break;
                 case PipeType.ThreeLeaf:
-                    arr = new Vector3[] { new Vector3(-1, 1), new Vector3(1, 1), new Vector3(0, 1, 1) };
+                    ends = new Vector3[] { new Vector3(-1, 1), new Vector3(1, 1), new Vector3(0, 1, 1) };
                     break;
             }
 
-            for (int i = 0; i < arr.Length; i++) {
-                arr[i] = interactionObject.transform.rotation * arr[i];
-                arr[i] += interactionObject.transform.position;
+            for (int i = 0; i < ends.Length; i++) {
+                ends[i] = interactionObject.transform.rotation * ends[i];
+                ends[i] += interactionObject.transform.position;
             }
-            renderer.positionCount = arr.Length;
-            renderer.SetPositions(arr);
+            renderer.positionCount = ends.Length;
+            renderer.SetPositions(ends);
+        }
+
+        public void SetSource(GameObject sourceObject, Vector3 hitPoint) {
+            // Quick hack. Should advantage grid system when that gets made.
+            int i = 0;
+            foreach (var end in ends) {
+                if (Vector3.Distance(end, hitPoint) <= 0.1f) {
+                    source = new Source(sourceObject, i);
+                    renderer.startColor = Color.blue;
+                    renderer.endColor = Color.blue;
+                } else {
+                    Physics.Raycast(end, interactionObject.transform.position - end, out RaycastHit nextSource);
+                    if (nextSource.collider.gameObject.TryGetComponent<Interaction>(out Interaction interaction) && interaction.behavior is WaterPipe w) {
+                        w.SetSource(interactionObject.gameObject, nextSource.point);
+                    }
+                }
+                i++;
+            }
+        }
+
+        public void ClearSource() {
+            source = null;
+            renderer.startColor = Color.white;
+            renderer.endColor = Color.white;
+            foreach (var end in ends) {
+                Physics.Raycast(end, interactionObject.transform.position - end, out RaycastHit nextSource);
+                if (nextSource.collider.gameObject.TryGetComponent<Interaction>(out Interaction interaction) && interaction.behavior is WaterPipe w) {
+                    w.ClearSource();
+                }
+            }
         }
 
         public override bool Update() {
