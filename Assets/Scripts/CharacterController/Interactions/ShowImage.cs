79cddecf3a5e886f3ccae729f8509cd3e1c943bb
Tyler K <tyly04@gmail.com>
2024-02-15 12:12:26 -0800

    ShowImage namespacing

--- Assets/Scripts/CharacterController/Interactions/ShowImage.cs
+++ Assets/Scripts/CharacterController/Interactions/ShowImage.cs
@@ -5,49 +5,49 @@ using UnityEngine;
 using UnityEngine.UI;
 using Utility;
 
-namespace Interactions.Behaviors {
-    public class ShowImageInteraction : InteractionBehavior {
-        public ShowImageInteraction(Interaction parent) : base(parent) { }
+namespace Interactions {
+    namespace Behaviors {
+        public class ShowImageInteraction : InteractionBehavior {
+            public ShowImageInteraction(Interaction parent) : base(parent) { }
 
-        public Sprite imageToShow;
-        bool isShowing = false;
-        public override void Interact() {
-            if (!isShowing) {
-                GameObject.FindGameObjectWithTag("Player").GetComponent<characterController>().moveEnabled = false;
-                isShowing = true;
-                ISingleton<ShowImage>.Instance.Show(imageToShow);
-            } else {
-                isShowing = false;
+            public Sprite imageToShow;
+            bool isShowing = false;
+            public override void Interact() {
+                if (!isShowing) {
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<characterController>().moveEnabled = false;
+                    isShowing = true;
+                    ISingleton<ShowImage>.Instance.Show(imageToShow);
+                } else {
+                    isShowing = false;
+                }
+            }
+
+            public override bool Update() {
+                return isShowing;
+            }
+
+            public override void EndInteraction() {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<characterController>().moveEnabled = true;
+                ISingleton<ShowImage>.Instance.Hide();
             }
         }
-
-        public override bool Update() {
-            return isShowing;
+    }
+    public class ShowImage : MonoBehaviour, ISingleton<ShowImage> {
+        Image img;
+        // Start is called before the first frame update
+        void Start() {
+            (this as ISingleton<ShowImage>).Initialize();
+            img = GetComponent<Image>();
         }
 
-        public override void EndInteraction() {
-            GameObject.FindGameObjectWithTag("Player").GetComponent<characterController>().moveEnabled = true;
-            ISingleton<ShowImage>.Instance.Hide();
+        public void Show(Sprite sprite) {
+            img.sprite = sprite;
+            img.enabled = true;
+            img.SetNativeSize();
         }
-    }
-}
-public class ShowImage : MonoBehaviour, ISingleton<ShowImage>
-{
-    Image img;
-    // Start is called before the first frame update
-    void Start()
-    {
-        (this as ISingleton<ShowImage>).Initialize();
-        img = GetComponent<Image>();
-    }
 
-    public void Show(Sprite sprite) {
-        img.sprite = sprite;
-        img.enabled = true;
-        img.SetNativeSize();
-    }
-
-    public void Hide() {
-        img.enabled = false;
+        public void Hide() {
+            img.enabled = false;
+        }
     }
 }
