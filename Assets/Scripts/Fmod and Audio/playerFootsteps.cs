d3ee49d488b3d5e31ab16d54c41e43e02fa4c476
Optimalystic <irkedibis@gmail.com>
2024-02-23 19:38:14 -0800

    Revert "Footstep audio script is running! Needs fine-tuning, but I'll talk to programming first"
    This reverts commit 997ed40a3d0409c49e1114c726bf6e6a17b9d711.

--- Assets/Scripts/Fmod and Audio/playerFootsteps.cs
+++ Assets/Scripts/Fmod and Audio/playerFootsteps.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class playerFootsteps : MonoBehaviour
 {
-
-    [SerializeField] public GameObject player;
-    private Vector3 lastPosition;
-    private Quaternion lastRotation;
-    private Vector3 lastScale;
-
-    private FMOD.Studio.EventInstance instance;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        lastPosition = player.transform.position;
-        lastRotation = player.transform.rotation;
-        lastScale = player.transform.localScale;
-        instance = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/footsteps");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.hasChanged)
-        {
-            instance.start();
-            lastPosition = player.transform.position;
-            lastRotation = player.transform.rotation;
-            lastScale = player.transform.localScale;
-        } 
-        else
-        {
-            instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        }
+        
     }
 }
