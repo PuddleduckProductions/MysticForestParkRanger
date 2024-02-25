1f783dd7f3133ee9c40527ce90e42cfbe9f68978
Tyler K <tyly04@gmail.com>
2024-02-25 09:36:31 -0800

    Velocity got busted in merge

--- Assets/Scripts/CharacterController/characterController.cs
+++ Assets/Scripts/CharacterController/characterController.cs
@@ -42,7 +42,13 @@ namespace Character {
         // Update is called once per frame
         void Update() {
             if (moveEnabled) {
-                c.SimpleMove(intendedMove * movementSpeed);
+                // adding acceleration
+                velocity += intendedMove * movementSpeed * Time.deltaTime;
+
+                //applying friction
+                velocity *= friction;
+
+                c.SimpleMove(velocity);
             }
 
             var xzVel = new Vector3(c.velocity.x, 0, c.velocity.z);
@@ -68,9 +74,6 @@ namespace Character {
 
         public Vector3 intendedMove {
             get {
-                float currRotationSpeed = rotationSpeed;
-                float currMoveSpeed = movementSpeed;
-
                 Quaternion simplifiedRot = Quaternion.AngleAxis(mainCamera.transform.eulerAngles.y, Vector3.up);
 
                 Vector3 simplifiedForward = relativeDirectionalMovement ? transform.forward : simplifiedRot * Vector3.forward;
@@ -80,22 +83,6 @@ namespace Character {
                 move.Normalize();
 
                 return move;
-
-
-                if (relativeDirectionalMovement) {
-                    transform.Rotate(Vector3.up, input.x * rotationSpeed * Time.deltaTime);
-                } else if (move != Vector3.zero) {
-                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move, transform.up), rotationSpeed * Time.deltaTime);
-                }
-                move.Normalize();
-
-                // adding acceleration
-                velocity += move * movementSpeed * Time.deltaTime;
-                
-                //applying friction
-                velocity *= friction;
-
-                c.SimpleMove(velocity);
             }
 
         }
