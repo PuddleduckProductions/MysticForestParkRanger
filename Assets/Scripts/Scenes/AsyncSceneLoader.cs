134176971abbaa1caa36de03840e677d8fe31a8c
Tyler K <tyly04@gmail.com>
2024-03-03 17:18:49 -0800

    Scene loading draft

---
+++ Assets/Scripts/Scenes/AsyncSceneLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AsyncSceneLoader : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Vector3 offset;
+    public Vector3 rotation;
+
+    private IEnumerator SceneLoading() {
+        var loader = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+        yield return loader;
+
+        var scene = SceneManager.GetSceneByName(sceneToLoad);
+        SceneManager.SetActiveScene(scene);
+        var objs = scene.GetRootGameObjects();
+        var parent = new GameObject("TransformParent");
+        foreach (var obj in objs ) {
+            if (obj.name == "PlayerObjects" || obj.name == "UI") {
+                obj.SetActive(false);
+            }
+            obj.transform.SetParent(parent.transform);
+        }
+        parent.transform.position += offset;
+        parent.transform.rotation = Quaternion.Euler(rotation);
+    }
+    private void OnTriggerEnter(Collider other) {
+        StartCoroutine(SceneLoading());
+    }
+}
