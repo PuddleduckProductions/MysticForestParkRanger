a1a0a5f9658d403d1575294d75b517bebb3445ac
Tyler K <tyly04@gmail.com>
2024-03-04 19:35:56 -0800

    Trash plants destroy trash tagged objects

---
+++ Assets/Scripts/Environment/TrashPlant.cs
@@ -0,0 +1,25 @@
+using Interactions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashPlant : MonoBehaviour
+{
+    public float radius = 1f;
+    // Temp code for now, we may want to change for later:
+    private void FixedUpdate() {
+        var colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var c in colliders) {
+            if (c.tag == "Trash") {
+                if (c.TryGetComponent(out Interaction i)) {
+                    i.EndInteraction();
+                }
+                Destroy(c.gameObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
