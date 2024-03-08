7ddf4665ad5c872c4b7bc74a1866ae7eae40e32c
~skysfall1ng~ <skythecreative@gmail.com>
2024-03-07 20:09:20 -0800

    testing ocean shader and imports

---
+++ Assets/Art Assets/[ WATER OBJECTS ]/Float.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    public Transform[] floaters;
+    public float underWaterDrag = 3f;
+    public float underWaterAngularDrag = 1f;
+    public float airDrag = 0f;
+    public float airAngularDrag = 0.05f;
+    public float floatingPower = 15f;
+
+    public Transform waterTransform;
+    public float water_height_offset;
+    float waterHeight = 0f;
+
+
+
+    Rigidbody rb;
+    bool underwater;
+    int floatersUnderWater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //InvokeRepeating("FakeWaveFloat", 0, 5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        waterHeight = waterTransform.position.y + water_height_offset;
+
+
+        floatersUnderWater = 0;
+        for (int i = 0; i < floaters.Length; i++)
+        {
+            float diff = floaters[i].position.y - waterHeight;
+            if (diff < 0)
+            {
+                rb.AddForceAtPosition(Vector3.up * floatingPower * Mathf.Abs(diff), floaters[i].position, ForceMode.Force);
+                floatersUnderWater += 1;
+                if (!underwater)
+                {
+                    underwater = true;
+                    SwitchState(true);
+                }
+            }
+        }
+        if (underwater && floatersUnderWater == 0)
+        {
+            underwater = false;
+            SwitchState(false);
+        }
+    }
+
+    void SwitchState(bool isUnderwater)
+    {
+        if (isUnderwater)
+        {
+            rb.drag = underWaterDrag;
+            rb.angularDrag = underWaterAngularDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+            rb.angularDrag = airAngularDrag;
+        }
+    }
+
+    public void FakeWaveFloat()
+    {
+        rb.AddForce(Vector3.down * 150, ForceMode.Force);
+    }
+}
