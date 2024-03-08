75fd3658c831b897eafa5d780c32085adde5e07e
Tyler K <tyly04@gmail.com>
2024-03-07 19:36:10 -0800

    Cameras on load update to player transform

--- Assets/Scripts/Scenes/AsyncSceneLoader.cs
+++ Assets/Scripts/Scenes/AsyncSceneLoader.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class AsyncSceneLoader : MonoBehaviour
 
     private IEnumerator SceneLoading() {
         var old = SceneManager.GetActiveScene();
+        var player = GameObject.FindGameObjectWithTag("Player");
+
         var loader = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
         yield return loader;
         var scene = SceneManager.GetSceneByName(sceneToLoad);
@@ -22,6 +25,16 @@ public class AsyncSceneLoader : MonoBehaviour
                 obj.SetActive(false);
             }
             obj.transform.SetParent(parent.transform);
+        }
+
+        var cameras = GameObject.FindObjectsOfType<CinemachineVirtualCamera>();
+        foreach (var camera in cameras) {
+            if (camera.LookAt != null) {
+                camera.LookAt = player.transform;
+            }
+            if (camera.Follow != null) {
+                camera.Follow = player.transform;
+            }
         }
         parent.transform.position += this.transform.TransformPoint(offset);
         parent.transform.rotation = Quaternion.Euler(rotation + this.transform.eulerAngles);
