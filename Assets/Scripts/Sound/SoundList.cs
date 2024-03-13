f53b0af583999137185bd107995131f4f2c5eee8
Optimalystic <irkedibis@gmail.com>
2024-03-12 22:35:46 -0700

    Experimenting with SoundList script, trying to define event paths inside the script. Currently commented out

--- Assets/Scripts/Sound/SoundList.cs
+++ Assets/Scripts/Sound/SoundList.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 [CreateAssetMenu(fileName = "SoundList", menuName ="ScriptableObjects/SoundList")]
 public class SoundList : ScriptableObject
@@ -15,6 +16,8 @@ public class SoundList : ScriptableObject
     //EventReference playerFootsteps;
     //EventReference dugongVoice;
     //EventReference dragOnSand;
+    //EventReference dragOnConcrete;
+    //EventReference grabObject;
 
     public EventReference this[string name] {
         get {
@@ -22,7 +25,19 @@ public class SoundList : ScriptableObject
         }
     }
 
+    //public void DefineEventRef(EventReference soundEvent, string path)
+    //{
+    //    soundEvent = RuntimeManager.PathToEventReference(path);
+    //    events.Add(soundEvent);
+    //}
+
     public void Init() {
+        //DefineEventRef(playerFootsteps, "event:/SFX/footsteps");
+        //DefineEventRef(dugongVoice, "event:/Voice/dugongVoice");
+        //DefineEventRef(dragOnSand, "event:/SFX/Grab&Drag/dragSand_Short");
+        //DefineEventRef(dragOnConcrete, "event:/SFX/Grab&Drag/dragConcrete_Short");
+        //DefineEventRef(grabObject, "event:/SFX/Grab&Drag/grabObject");
+
         foreach (var e in events) {
             if (!eventDict.ContainsKey(e.Guid.ToString())) {
                 eventDict.Add(e.Guid.ToString(), e);
